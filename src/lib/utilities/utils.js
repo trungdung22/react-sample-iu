846a27bb72ce8d10413407664e0f429b36e01631
Tri Nguyen Trong <ngttri96@gmail.com>
2022-01-07 22:55:26 +0700

    commit code

--- src/lib/utilities/utils.js
+++ src/lib/utilities/utils.js
@@ -1,6 +1,16 @@
 
 import { getBalance } from "lib/program/lottery-commands";
 import { IS_CONNECT, HOST_NAME } from 'data/constants';
+import { SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID, TOKEN_PROGRAM_ID } from "./id";
+const {
+    SystemProgram,
+    PublicKey,
+    sendAndConfirmTransaction,
+    Transaction,
+    SYSVAR_RENT_PUBKEY,
+    TransactionInstruction,
+    Account
+} = require('@solana/web3.js');
 
 export const convertLamport = async (lamports) => {
     const requestOptions = {
@@ -11,10 +21,10 @@ export const convertLamport = async (lamports) => {
     };
     const response = await fetch(`${HOST_NAME}/api/rate-converter?lamports=${lamports}`, requestOptions);
     const data = await response.json();
-    return { usdt: data["usdt"], sol: data["sol"]};
+    return { usdt: data["usdt"], sol: data["sol"] };
 }
 
-export const convertUSDT= async (amount) => {
+export const convertUSDT = async (amount) => {
     const requestOptions = {
         method: 'GET',
         headers: {
@@ -23,8 +33,8 @@ export const convertUSDT= async (amount) => {
     };
     const response = await fetch(`${HOST_NAME}/api/rate-converter-usdt?amount=${amount}`, requestOptions);
     const data = await response.json();
-    return { lamports: data["lamports"]};
-} 
+    return { lamports: data["lamports"] };
+}
 
 export const priceTicket = async () => {
     const requestOptions = {
@@ -42,12 +52,12 @@ export const registerMilipadPlayer = async (playerPubkey, missions, code) => {
     const requestOptions = {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json',
+            'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ 
-          playerPubkey: playerPubkey, 
-          missions: missions,
-          code: code
+        body: JSON.stringify({
+            playerPubkey: playerPubkey,
+            missions: missions,
+            code: code
         })
     };
     const response = await fetch(`${HOST_NAME}/api/milli-pads/register`, requestOptions);
@@ -69,15 +79,15 @@ export const getMillipads = async () => {
 export const updateMiliPadPlayer = async (amount, code, playerPubkey) => {
     const requestOptions = {
         method: 'POST',
-        headers: { 
+        headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ 
-            amount: amount, 
+        body: JSON.stringify({
+            amount: amount,
             code: code,
             playerPubkey: playerPubkey
         })
-        
+
     };
     const response = await fetch(`${HOST_NAME}/api/milli-pads/update-amount`, requestOptions);
     return response;
@@ -87,15 +97,15 @@ export const insertBulkTicket = async (gamePubkey, playerPubkey, tickets) => {
     //post add ticket
     const requestOptions = {
         method: 'POST',
-        headers: { 
+        headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ 
-            gamePubkey: gamePubkey, 
+        body: JSON.stringify({
+            gamePubkey: gamePubkey,
             tickets: tickets,
             playerPubkey: playerPubkey
         })
-        
+
     };
     const response = await fetch(`${HOST_NAME}/api/ticket/insert-bulk`, requestOptions);
     return response;
@@ -117,7 +127,7 @@ export const fetchPlayerAccount = async (publicKey) => {
     if (publicKey === undefined || publicKey === '') {
         return {
             balanceUSDT: 0,
-            balanceSOL: 0, 
+            balanceSOL: 0,
             lamportUnit: 0,
             publicKey: ''
         };
@@ -128,7 +138,7 @@ export const fetchPlayerAccount = async (publicKey) => {
     const ticketPrice = await priceTicket()
     return {
         balanceUSDT: balanceConvert['usdt'],
-        balanceSOL: balanceConvert['sol'], 
+        balanceSOL: balanceConvert['sol'],
         lamportUnit: ticketPrice['lamports'],
         publicKey: publicKey
     };
@@ -138,12 +148,12 @@ export const updateMissionPlayer = async (playerPubkey, missions, code) => {
     const requestOptions = {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json',
+            'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ 
-          playerPubkey: playerPubkey, 
-          mission: missions,
-          code: code
+        body: JSON.stringify({
+            playerPubkey: playerPubkey,
+            mission: missions,
+            code: code
         })
     };
     const response = await fetch(`${HOST_NAME}/api/milli-pads/update-mission`, requestOptions);
@@ -154,9 +164,9 @@ export const updateJoinWhiteListUser = async (playerPubkey) => {
     const requestOptions = {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json',
+            'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ 
+        body: JSON.stringify({
             playerPubkey: playerPubkey
         })
     };
@@ -170,23 +180,23 @@ export async function getOrCreateTokenAccountInstruction(connection, walletPubke
         await findProgramAddress(
             [
                 walletPubkey.toBuffer(),
-                programIds().token.toBuffer(),
-                toPublicKey(mintKey).toBuffer(),
+                TOKEN_PROGRAM_ID,
+                mintKey,
             ],
-            programIds().associatedToken,
+            SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID,
         )
     )[0];
     const accountKey = new PublicKey(tokenKey);
-    const account = await connection.getAccountInfo(accountKey); 
-    
+    const account = await connection.getAccountInfo(accountKey);
+
     if (account === null) {
-        const instructions = []; 
+        const instructions = [];
         createAssociatedTokenAccountInstruction(
             instructions,
-            toPublicKey(tokenKey),
+            accountKey,
             accountSigner.publicKey,
             walletPubkey,
-            toPublicKey(mintKey),
+            mintKey,
         );
         const tx = new Transaction().add(...instructions);
         await sendAndConfirmTransaction(
@@ -196,5 +206,66 @@ export async function getOrCreateTokenAccountInstruction(connection, walletPubke
             { commitment: 'singleGossip', preflightCommitment: 'singleGossip', }
         )
     }
-    return toPublicKey(tokenKey);
+    return accountKey.publicKey;
 }
+
+function createAssociatedTokenAccountInstruction(
+    instructions,
+    associatedTokenAddress,
+    payer,
+    walletAddress,
+    splTokenMintAddress,
+) {
+    const keys = [
+        {
+            pubkey: payer,
+            isSigner: true,
+            isWritable: true,
+        },
+        {
+            pubkey: associatedTokenAddress,
+            isSigner: false,
+            isWritable: true,
+        },
+        {
+            pubkey: walletAddress,
+            isSigner: false,
+            isWritable: false,
+        },
+        {
+            pubkey: splTokenMintAddress,
+            isSigner: false,
+            isWritable: false,
+        },
+        {
+            pubkey: SystemProgram.programId,
+            isSigner: false,
+            isWritable: false,
+        },
+        {
+            pubkey: TOKEN_PROGRAM_ID,
+            isSigner: false,
+            isWritable: false,
+        },
+        {
+            pubkey: SYSVAR_RENT_PUBKEY,
+            isSigner: false,
+            isWritable: false,
+        },
+    ];
+    instructions.push(
+        new TransactionInstruction({
+            keys,
+            programId: SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID,
+            data: Buffer.from([]),
+        }),
+    );
+}
+
+const findProgramAddress = async (
+    seeds,
+    programId,
+) => {
+    const result = await PublicKey.findProgramAddress(seeds, programId);
+    return [result[0].toBase58(), result[1]];
+};

468db1706124c3cb20d144c2befc71d77c841b1c
Tri Nguyen Trong <ngttri96@gmail.com>
2022-01-17 15:35:00 +0700

    update your tickets on lottery view

--- src/components/astoms/modalSection/viewTicket/indext.tsx
+++ src/components/astoms/modalSection/viewTicket/indext.tsx
@@ -1,37 +1,44 @@
-import React, { useState } from 'react';
+import { HOST_NAME } from 'data/constants';
+import React, { useEffect, useState } from 'react';
 import useStyles from './styles';
 type Props = {
   dataSendViewTicket: any,
   dataGiveViewTicket: (getDataViewTicket: boolean) => void,
 }
-const ViewTicket: React.FC<Props> = ({dataSendViewTicket, dataGiveViewTicket}) => {
+const ViewTicket: React.FC<Props> = ({ dataSendViewTicket, dataGiveViewTicket }) => {
   const classes = useStyles();
   const [showTooltipConnect, setShowTooltipConnect] = useState(false);
-  const [data, setData] = useState({
-    tickets: dataSendViewTicket.next_round.your_ticket,
-  })
+  const [tickets, setTickets] = useState(dataSendViewTicket.next_round.your_ticket)
+
+  const [nftTickets, setNftTickets] = useState([])
 
   const [activeLabel, setActiveLabel] = useState('lottery');
 
-  const ticketsLottery = [
-    // [1, 1, 1, 1, 1, 1],
-    // [1, 1, 1, 1, 1, 1],
-    // [1, 1, 1, 1, 1, 1],
-    // [1, 1, 1, 1, 1, 1],
-  ];
+  useEffect(() => {
+    if (!dataSendViewTicket.is_connect)
+      return;
 
-  const ticketsNFT = [
-    [2, 2, 2],
-    [2, 2, 2, 2],
-    [2, 2, 2, 2, 2],
-    [2, 2, 2, 2, 2, 2],
-  ];
+    const userPubkey = window.sessionStorage.getItem('publicKey');
+    fetch(`${HOST_NAME}/api/nft-ticket?isSole=true&user_pubkey=${userPubkey}`)
+      .then(async res => {
+        let data = await res.json();
+        if (!res.ok) {
+          const error = (data && data.message) || res.statusText;
+          return Promise.reject(error);
+        }
+        let nfts = data.items.map(el => el.roll_nums);
+        setNftTickets(nfts)
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, [])
 
-  const [dataRender, setDataRender] = useState(ticketsLottery);
-  
+  const [dataRender, setDataRender] = useState(tickets);
+
   const handleRenderTicket = (data: any) => {
     if (data.length > 0 && window.sessionStorage.getItem('data_connect') === 'true') {
-      return(
+      return (
         data.map((el: [], index: number) => (
           <dl key={index}>
             <dt>#{index < 9 ? `00${index + 1}` : `0${index + 1}`}</dt>
@@ -49,24 +56,24 @@ const ViewTicket: React.FC<Props> = ({dataSendViewTicket, dataGiveViewTicket}) =
       )
     }
 
-    return(
+    return (
       <p className='text-12 font-bold text-pink-D47DFF mt-2'>You have no ticket</p>
     )
   }
-  
+
   return (
     <>
       <div className='p-4 pt-2'>
         <ul className='mb-2'>
           <li className={`cursor-pointer transition-all text-14 font-semibold rounded-5 w-115 inline-flex justify-center items-center h-32px mr-4 ${activeLabel === 'lottery' ? 'bg-pink-8C24BF' : 'hover:bg-gray-575757-50 hover:text-gray-F9F9F9 text-gray-A9A9A9'}`}
             onClick={() => {
-              setDataRender(ticketsLottery);
+              setDataRender(tickets);
               setActiveLabel('lottery');
             }}
           >Lottery tickets</li>
           <li className={`cursor-pointer transition-all text-14 font-semibold rounded-5 w-90 inline-flex justify-center items-center h-32px ${activeLabel === 'nft' ? 'bg-pink-8C24BF' : 'hover:bg-gray-575757-50 hover:text-gray-F9F9F9 text-gray-A9A9A9'}`}
             onClick={() => {
-              setDataRender(ticketsNFT);
+              setDataRender(nftTickets);
               setActiveLabel('nft');
             }}
           >NFT tickets</li>
@@ -83,12 +90,12 @@ const ViewTicket: React.FC<Props> = ({dataSendViewTicket, dataGiveViewTicket}) =
               setShowTooltipConnect(false);
             }}
             onMouseEnter={() => {
-              if(window.sessionStorage.getItem('data_connect') !== 'true') {
+              if (window.sessionStorage.getItem('data_connect') !== 'true') {
                 setShowTooltipConnect(true);
               }
             }}
           >Buy ticket</p>
-          { showTooltipConnect && <p className='absolute bottom-full left-1/2 transform -translate-x-1/2 -translate-y-2 z-100 border border-solid border-pink-A819FA bg-gray-151515 rounded-5 text-center text-14 md:text-16 py-1 w-44 text-pink-A819FA font-medium'>Connect wallet first</p> }
+          {showTooltipConnect && <p className='absolute bottom-full left-1/2 transform -translate-x-1/2 -translate-y-2 z-100 border border-solid border-pink-A819FA bg-gray-151515 rounded-5 text-center text-14 md:text-16 py-1 w-44 text-pink-A819FA font-medium'>Connect wallet first</p>}
         </div>
       </div>
     </>
